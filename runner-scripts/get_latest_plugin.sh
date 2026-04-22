3e45011c0eb7a41de42456b145674153f08fdcb5
M.Angel Juan <m.angel.juan@gmail.com>
2026-04-22 14:22:21 +0200

    Translate and allow to set fixed version for download

--- runner-scripts/get_latest_plugin.sh
+++ runner-scripts/get_latest_plugin.sh
@@ -1,24 +1,33 @@
-API_URL="https://api.github.com/repos/Acly/krita-ai-diffusion/releases/latest"
+#!/bin/bash
 
-echo "Consultando GitHub..."
+FORCE_VERSION_TAG=$1
+API_URL="https://api.github.com/repos/Acly/krita-ai-diffusion"
+
+if [ -n "$FORCE_VERSION" ]; then
+    echo Getting plugin for tag $FORCE_VERSION
+    API_URL="$API_URL/releases/tags/$FORCE_VERSION_TAG"
+else
+    echo Getting latest version plugin
+    API_URL="$API_URL/releases/latest"
+fi
+echo "Checking GitHub..."
 curl -sL -H "User-Agent: Linux" "$API_URL" | jq -r '.tag_name + " " + .assets[0].browser_download_url' > .download_info
 
 read -r VERSION DOWNLOAD_URL <<< $(cat .download_info)
 
-# 3. Mostrar la versión
 echo "---------------------------------------"
-echo "Versión encontrada: $VERSION"
-echo "URL de descarga:    $DOWNLOAD_URL"
+echo "Found version: $VERSION"
+echo "Download URL:  $DOWNLOAD_URL"
 echo "---------------------------------------"
 
 FILENAME=$(basename "$DOWNLOAD_URL")
 
 mkdir -p {package,decompressed}
-echo "Descargando $FILENAME..."
+echo "Downloading $FILENAME..."
 cd package
 curl -L -# -O "$DOWNLOAD_URL"
 cd ../decompressed
-echo "Descomprimiendo..."
+echo "Descompressing..."
 unzip -qo "../package/$FILENAME" 
 cd ..
-echo "¡Listo!"
+echo "Done!"
